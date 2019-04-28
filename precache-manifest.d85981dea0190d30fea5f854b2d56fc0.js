b7bb3e786e99f8e0dd03942e4c7380dc3075211a
tammyleeseattle <tammylee84@gmail.com>
2019-04-28 12:49:44 -0700

    Updates

---
+++ precache-manifest.d85981dea0190d30fea5f854b2d56fc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fcaaa60b4cdde630b58338051011b98",
+    "url": "/clicky-game/clicky-game/index.html"
+  },
+  {
+    "revision": "13e40b4951cf585e5e02",
+    "url": "/clicky-game/clicky-game/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/clicky-game/clicky-game/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "13e40b4951cf585e5e02",
+    "url": "/clicky-game/clicky-game/static/js/main.d2cf2c9d.chunk.js"
+  },
+  {
+    "revision": "94d92a0c38b2dcad00b0",
+    "url": "/clicky-game/clicky-game/static/js/runtime~main.fcee8508.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
